2617d68cc178b9895aef9cefacc32402d9a72c1e
dimagadev <lobodindimaga@gmail.com>
2022-08-24 18:08:20 +0300

    1565656565

--- test.js
+++ test.js
@@ -1,4 +1,12 @@
-const obj = {
-	a: 's',
-	b: 'h',
-};
+function showMessage(from, text) {
+	from = '*' + from + '*'; // немного украсим "from"
+
+	alert(from + ': ' + text);
+}
+
+let from = 'Аня';
+
+showMessage(from, 'Привет'); // *Аня*: Привет
+
+// значение "from" осталось прежним, функция изменила значение локальной переменной
+alert(from); // Аня
