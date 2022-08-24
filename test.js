9b8d9a3239e2487cd30e8ac0304e12f101be71e1
dimagadev <lobodindimaga@gmail.com>
2022-08-24 18:08:50 +0300

    15

--- test.js
+++ test.js
@@ -10,3 +10,19 @@ showMessage(from, 'Привет'); // *Аня*: Привет
 
 // значение "from" осталось прежним, функция изменила значение локальной переменной
 alert(from); // Аня
+
+function checkAge(age) {
+	if (age > 18) {
+		return true;
+	} else {
+		return confirm('А родители разрешили?');
+	}
+}
+
+let age = prompt('Сколько вам лет?', 18);
+
+if (checkAge(age)) {
+	alert('Доступ получен');
+} else {
+	alert('Доступ закрыт');
+}
