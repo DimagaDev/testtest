e16abeccd5546a882df4bc1b2a74b5a959253158
dimagadev <lobodindimaga@gmail.com>
2022-08-24 18:07:13 +0300

    sdadas

--- test.js
+++ test.js
@@ -2,3 +2,20 @@ const obj = {
 	a: 's',
 	b: 'h',
 };
+
+const sum = (a, b, c, d) => {
+	return a + b + c + d;
+};
+
+function myFunc(theObject) {
+	theObject.make = 'Toyota';
+}
+
+var mycar = { make: 'Honda', model: 'Accord', year: 1998 };
+var x, y;
+
+x = mycar.make; // x получает значение "Honda"
+
+myFunc(mycar);
+y = mycar.make; // y получает значение "Toyota"
+// (свойство было изменено функцией)
