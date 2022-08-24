dc025ff12ac0d41b943e879b4ec0ad0dcc457f45
dimagadev <lobodindimaga@gmail.com>
2022-08-24 18:07:32 +0300

    121212

--- test.js
+++ test.js
@@ -19,3 +19,16 @@ x = mycar.make; // x получает значение "Honda"
 myFunc(mycar);
 y = mycar.make; // y получает значение "Toyota"
 // (свойство было изменено функцией)
+
+function map(f, a) {
+	var result = []; // Создаём новый массив
+	var i; // Объявляем переменную
+	for (i = 0; i != a.length; i++) result[i] = f(a[i]);
+	return result;
+}
+var f = function (x) {
+	return x * x * x;
+};
+var numbers = [0, 1, 2, 5, 10];
+var cube = map(f, numbers);
+console.log(cube);
